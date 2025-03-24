3f655e299a569ab1fa3c4480ffa716e9cacb1a40
Robin Zollner <code@bravegrumpy.com>
2025-03-24 17:29:18 -0400

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import pageInsight from 'astro-page-insight';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    pageInsight({
+      lh: {
+        weight: 0,
+        breakPoint: 1024,
+      },
+      firstFetch: "none",
+      cache: true,
+      build: {
+        bundle: true,
+        showOnLoad: true
+      }
+    }), 
+    icon()
+  ]
+});
