439f380510ca03507f51dc35d2440a4b3ae5ceff
Robin Zollner <code@bravegrumpy.com>
2025-03-25 21:09:55 -0400

    recreated previous commit. Figured out the big issue was using both object import and component on the same markdown file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
+import sitemap from '@astrojs/sitemap';
+
+import pageInsight from 'astro-page-insight';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://merther.vercel.app',
+  integrations: [
+    icon(), 
+    sitemap(), 
+    pageInsight()
+  ]
+});
