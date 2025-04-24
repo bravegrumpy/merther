20c429c92fe7e97b73bea4b8577816c1b8c94d30
Robin Zollner <code@bravegrumpy.com>
2025-04-24 10:34:45 -0400

    minimal rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,6 +3,7 @@ import rss from '@astrojs/rss';
 export function GET(context) {
     return rss({
         title: 'Merther Fanfic',
-        description: 'A collection of Merlin fan works.'
+        description: 'A collection of Merlin fan works.',
+        site: context.site
     })
 }
