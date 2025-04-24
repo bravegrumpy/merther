fcc6fa9685cd3b6573a6f9bf5f42fab41b1e4e83
Robin Zollner <code@bravegrumpy.com>
2025-04-24 03:21:46 -0400

    including content in RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,24 @@
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+const parser = new MarkdownIt();
+
+export async function GET(context){
+    const misunderstood = await getCollection('misunderstood');
+    return rss({
+        title: "Misunderstood | Merther",
+        description: "A sad fanfic based in Merther",
+        site: context.site,
+        trailingSlash: false,
+        items: misunderstood.map((chapter) => ({
+            title: chapter.data.title,
+            link: `/misunderstood/simple/${chapter.id}/`,
+            content: sanitizeHtml(parser.render(chapter.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
+            ...chapter.data
+        })),
+        customData: `<language>en-us</language>`,
+    });
+}
