69cd9434e6430503daf0cac68b3bfded7b888c63
Robin Zollner <code@bravegrumpy.com>
2025-04-23 23:25:18 -0400

    switching to content collection

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content"; 
+
+import { glob } from "astro/loaders"
+
+const misunderstood = defineCollection({
+    loader: glob({ pattern: ["*.{md,mdx}", "!index**"], base: "./src/misunderstood/chapters" }),
+    schema: z.object({
+        id: z.string(),
+        href: z.string(),
+        text: z.string(),
+        chapter: z.number(),
+        title: z.string(),
+        tags: z.optional(z.array(z.string()))
+    })
+});
+
+export const collections = { misunderstood }
