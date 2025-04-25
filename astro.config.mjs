9cdd09b04aa0271b5a38fb1da8a416b12367e621
Robin Zollner <code@bravegrumpy.com>
2025-04-25 13:01:15 -0400

    chnage to pdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -106,7 +106,7 @@ export default defineConfig({
         throwOnFail: true,
         pdf: {
           format: "LETTER",
-          printBackground: true
+          printBackground: false
         }
       }
     }
