9f5546e7dde1ef95e33f3edf34c57debd4e51957
Robin Zollner <code@bravegrumpy.com>
2025-04-27 19:06:01 -0400

    adding  contentful (This should not work correctly)

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,9 @@
 interface ImportMetaEnv {
     readonly XATA_API_KEY: string;
     readonly XATA_BRANCH?: string;
+    readonly CONTENTFUL_SPACE_ID?: string;
+    readonly CONTENTFUL_DELIVERY_TOKEN?: string;
+    readonly CONTENTFUL_PREVIEW_TOKEN?: string;
   }
   
   interface ImportMeta {
