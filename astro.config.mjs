c10f54f8da86b41fe573ecf9d3faccc0af718d14
Robin Zollner <code@bravegrumpy.com>
2025-04-27 22:29:35 -0400

    back to ssg

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,8 +33,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 import aiRobotsTxt from 'astro-ai-robots-txt';
 
-import awsAmplify from 'astro-aws-amplify'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mertherfanfic.neocities.org',
@@ -78,6 +76,5 @@ export default defineConfig({
 
   trailingSlash: 'always',
   
-  adapter: awsAmplify(),
   output: 'static',
 });
