13d8f86ea634aa32b9937c7596219e11b4c693c6
Robin Zollner <code@bravegrumpy.com>
2025-05-01 17:07:46 -0400

    adding images api

---
+++ src/pages/api/images/[image].png.ts
@@ -0,0 +1,12 @@
+export async function GET ({ params, request }: {params: any, request: any}) {
+    const response = await fetch(
+        `https://cdn.bravegrumpy.com/AO3Styles/images/${params.image}.png`
+    );
+    const buffer = Buffer.from(await response.arrayBuffer());
+
+    return new Response(buffer, {
+        headers: {
+            "Content-Type": "image/png"
+        }
+    })
+}
