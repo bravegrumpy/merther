304cab6ad3907c715cf8463a645228ef542789b1
Robin Zollner <code@bravegrumpy.com>
2025-04-30 20:31:43 -0400

    adding svelte

---
+++ src/components/svelte/SelectTheme.svelte
@@ -0,0 +1,34 @@
+<script>
+let themes = [
+        {
+            file: 'main',
+            text: 'Simple',
+            strategy: 'vanilla',
+            lang: 'CSS',
+            id: '00'
+        },
+        {
+            file: 'app',
+            text: 'Tailwind',
+            strategy: 'tailwind',
+            lang: 'Tailwind 4.0',
+            id: '01'
+        },
+        {
+            file: 'print',
+            text: 'Print Preview',
+            strategy: 'css',
+            lang: 'CSS',
+            id: '02'
+        }
+    ];
+
+    let selected = $state(themes[0])
+</script>
+<svelte:head>
+    <link rel="stylesheet" href="" /></svelte:head>
+<select bind:value={selected}>
+    {#each themes as theme (theme.id)}
+        <option value={theme}>{theme.text}</option>
+    {/each}
+</select>
