b56acc35b2736a8263f41b66548dbf06658de83e
Robin Zollner <code@bravegrumpy.com>
2026-01-03 22:10:00 -0500

    adding cdhanges to alt copies

--- ssg/astro.config.mjs
+++ ssg/astro.config.mjs
@@ -40,6 +40,24 @@ import aiRobotsTxt from 'astro-ai-robots-txt';
 export default defineConfig({
   // site: 'https://merther.bravegrumpy.com',
   site: 'https://mertherfanfic.neocities.org',
+  // security: {
+  //   checkOrigin: true,
+  //   allowedDomains: [
+  //     {
+  //       hostname: "*.testing-cors-self.vercel.app",
+  //       protocol: 'https'
+  //     },
+  //     {
+  //       hostname: '**.ncase.me',
+  //       protocol: 'https'
+  //     },
+  //     {
+  //       hostname: '**.bravegrumpy.com',
+  //       protocol: 'https'
+  //     }
+  //   ],
+  // },
+
 
   image: {
     domains: ["bravegrumpy.com"],
--- ssr/astro.config.mjs
+++ ssr/astro.config.mjs
@@ -40,6 +40,25 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://merther.bravegrumpy.com',
   // site: 'https://mertherfanfic.neocities.org',
+  security: {
+    checkOrigin: true,
+    allowedDomains: [
+      {
+        hostname: "*.testing-cors-self.vercel.app",
+        protocol: 'https'
+      },
+      {
+        hostname: '**.ncase.me',
+        protocol: 'https'
+      },
+      {
+        hostname: '**.bravegrumpy.com',
+        protocol: 'https'
+      }
+    ],
+  },
+
+
 
   image: {
     domains: ["bravegrumpy.com"],
